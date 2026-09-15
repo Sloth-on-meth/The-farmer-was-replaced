2385d56e139c6421748516f3d27eb7d3ff15a713
sloth_on_meth <info.sbphotographics@gmail.com>
2026-09-16 01:46:38 +0200

    67

--- f0.py
+++ f0.py
@@ -1,62 +1,79 @@
 size = get_world_size()
-pumpkin_split = size // 2
+split = size // 2
+still_growing = True
 
-while True:
-	#rollist = [1,2,3,4,5,6,7,8,9,10]
-	
-
+def sweep(action):
 	for row in range(size):
-		
 		for col in range(size):
-#			hat_roll = random()
-#			#print(hat_roll)
-#			if hat_roll < 0.25:
-#				change_hat(Hats.Straw_Hat)
-#			elif hat_roll < 0.5:
-#				change_hat(Hats.Tree_Hat)
-#			elif hat_roll < 0.75:
-#				change_hat(Hats.Carrot_Hat)
-#			else:
-#				change_hat(Hats.Pumpkin_Hat)
-
-			#if num_items(Items.Fertilizer) > 0:
-			#		use_item(Items.Fertilizer)
-			if get_water() < 0.2:
-				
-					use_item(Items.Water)
-			use_item(Items.Fertilizer)
-			harvest()
-			if can_harvest():
-				harvest()
-
-			if get_entity_type() == Entities.Dead_Pumpkin:
-				harvest()
-
-			if col >= pumpkin_split:
-				if get_ground_type() == Grounds.Grassland:
-					till()
-
-				if get_entity_type() == None:
-					plant(Entities.Pumpkin)
-			else:
-				is_tree_tile = row % 2 == 0 and col % 2 == 0
-				is_carrot_tile = row % 2 == 1 and col % 2 == 1
-
-				if is_tree_tile or is_carrot_tile:
-					if get_ground_type() == Grounds.Grassland:
-						till()
-
-					if get_entity_type() == None:
-						if is_tree_tile:
-							plant(Entities.Tree)
-						else:
-							plant(Entities.Carrot)
-
+			action(row, col)
 			if col < size - 1:
 				if row % 2 == 0:
 					move(East)
 				else:
 					move(West)
-
 		if row < size - 1:
 			move(South)
+
+def water_and_fertilize():
+	if get_water() < 0.2:
+		if num_items(Items.Water) > 0:
+			use_item(Items.Water)
+	if get_entity_type() != None and not can_harvest() and num_items(Items.Fertilizer) > 0:
+		use_item(Items.Fertilizer)
+
+def grow(entity):
+	if can_harvest():
+		harvest()
+	if get_ground_type() == Grounds.Grassland:
+		till()
+	if get_entity_type() == None:
+		plant(entity)
+
+def pumpkin_tile(row, col):
+	global still_growing
+	if col < split:
+		return
+	entity = get_entity_type()
+	if entity == Entities.Dead_Pumpkin or entity == None:
+		if get_ground_type() == Grounds.Grassland:
+			till()
+		plant(Entities.Pumpkin)
+		still_growing = True
+	elif not can_harvest():
+		still_growing = True
+
+def harvest_pumpkins(row, col):
+	if col < split:
+		return
+	if get_entity_type() == Entities.Dead_Pumpkin:
+		harvest()
+	elif can_harvest():
+		harvest()
+
+def farm_tile(row, col):
+	water_and_fertilize()
+	if col >= split:
+		pumpkin_tile(row, col)
+	elif row % 2 == 0 and col % 2 == 0:
+		grow(Entities.Tree)
+	elif row % 2 == 1 and col % 2 == 1:
+		grow(Entities.Carrot)
+	elif row % 2 == 0:
+		grow(Entities.Bush)
+	else:
+		if can_harvest():
+			harvest()
+
+while True:
+	confirmed = 0
+	attempts = 0
+	while confirmed < 2 and attempts < 8:
+		still_growing = False
+		sweep(farm_tile)
+		attempts += 1
+		if still_growing:
+			confirmed = 0
+		else:
+			confirmed += 1
+
+	sweep(harvest_pumpkins)
